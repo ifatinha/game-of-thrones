68784231179bd1936f655011c947db8651b1ed84
ifatinha <ifatinha99@gmail.com>
2024-01-21 19:01:28 -0300

    section intro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 /** open menu mobile */
 const openMenuMobileBtn = document.querySelector(".menu-mobile");
-const closeMenuMobileBtn = document.querySelector(".close-mobile-btn");
+const closeMenuMobileBtn = document.querySelector("#closeMenuMobileBtn");
 
 const menuOverlay = document.querySelector(".menu-overlay");
 
 function openMenuMobile() {
     menuOverlay.classList.remove("close-menu-mobile");
-    menuOverlay.classList.toggle("open-menu-mobile");
+    menuOverlay.classList.add("open-menu-mobile");
 }
 
 function closeMenuMobile() {
@@ -16,3 +16,27 @@ function closeMenuMobile() {
 
 openMenuMobileBtn.addEventListener("click", openMenuMobile);
 closeMenuMobileBtn.addEventListener("click", closeMenuMobile);
+
+/** open modal video trailer */
+const whactTrailerBtn = document.querySelector("#whactTrailerBtn");
+const closeTrailerBtn = document.querySelector("#closeTrailerBtn");
+const modalTrailer = document.querySelector("#modalTrailer");
+
+function openModalTrailer() {
+    modalTrailer.style.display = "flex";
+}
+
+function closeModalTrailer() {
+    const trailerVideo = document.querySelector("#trailer");
+    modalTrailer.style.display = "none";
+    trailerVideo.pause();
+}
+
+whactTrailerBtn.addEventListener("click", openModalTrailer);
+closeTrailerBtn.addEventListener("click", closeModalTrailer);
+
+window.onclick = function (event) {
+    if (event.target == modalTrailer) {
+        modalTrailer.style.display = "none";
+    }
+}
