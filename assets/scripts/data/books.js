80b2b17bd0a5b50f4f9ed635bf01eb7043a917a1
Fatima Ferreira <ifatinha99@gmail.com>
2024-10-12 18:52:44 -0300

    atualizações

---
+++ assets/scripts/data/books.js
@@ -0,0 +1,71 @@
+export const books = [
+  {
+    id: 1,
+    title: "A guerra dos tronos",
+    year: "1996",
+    description:
+      "No primeiro livro, o poder político e as traições começam a desenrolar o destino dos Sete Reinos. No Norte, o inverno se aproxima, enquanto no Sul, famílias nobres lutam pelo Trono de Ferro. O início de uma batalha épica por poder, honra e sobrevivência.",
+    image: "assets/images/book-01.jpg",
+    bgImage: "assets/images/book-01-bg.jpg",
+  },
+
+  {
+    id: 2,
+    title: "A Fúria dos Reis",
+    year: "1998",
+    description:
+      "Com a morte do rei, os reinos mergulham em guerra civil. Vários pretendentes ao trono surgem, cada um reivindicando sua legitimidade. Enquanto as batalhas devastam Westeros, forças antigas e misteriosas começam a se mover além da Muralha, prenunciando uma ameaça muito maior.",
+    image: "assets/images/book-02.jpg",
+    bgImage: "assets/images/book-02-bg.jpg",
+  },
+
+  {
+    id: 3,
+    title: "A Tormenta das espadas",
+    year: "2000",
+    description:
+      "O terceiro livro marca o clímax da guerra dos tronos. Alianças são formadas e desfeitas em meio a batalhas devastadoras, enquanto traições chocantes redefinem o destino de grandes famílias. O Casamento Vermelho, um dos momentos mais brutais e inesquecíveis da saga, ocorre nesta fase da guerra.",
+    image: "assets/images/book-03.jpg",
+    bgImage: "assets/images/book-03-bg.jpg",
+  },
+
+  {
+    id: 4,
+    title: "O festim dos corvos",
+    year: "2005",
+    description:
+      "Com muitos dos grandes líderes mortos, o quarto livro foca nos efeitos da guerra sobre os sobreviventes. Westeros está quebrado e vulnerável, com seu povo devastado pela fome e pelo luto, enquanto novos desafios e ambições emergem, moldando o futuro do reino.",
+    image: "assets/images/book-04.jpg",
+    bgImage: "assets/images/book-04-bg.jpg",
+  },
+
+  {
+    id: 5,
+    title: "A Dança dos Dragões",
+    year: "2011",
+    description:
+      "Enquanto Daenerys Targaryen luta para manter o controle sobre seus dragões e a cidade de Meereen, forças no Norte se preparam para uma guerra decisiva. Jon Snow enfrenta desafios imensos na Patrulha da Noite, enquanto o inverno finalmente começa a se instalar, trazendo caos, morte e escuridão ao continente.",
+    image: "assets/images/book-05.jpg",
+    bgImage: "assets/images/book-05-bg.jpg",
+  },
+
+  {
+    id: 6,
+    title: "Os Ventos do Inverno",
+    year: "Livro Futuro",
+    description:
+      "No sexto livro, o inverno atinge Westeros com força total. As forças do Norte enfrentam os mortos-vivos, enquanto no Sul, a guerra pelo Trono de Ferro continua. Jon Snow enfrenta decisões impossíveis, Daenerys lida com o caos em Meereen, e o futuro de Westeros nunca esteve tão incerto. O frio e a escuridão ameaçam engolir tudo.",
+    image: "assets/images/book-06.jpg",
+    bgImage: "assets/images/book-06-bg.jpg",
+  },
+
+  {
+    id: 7,
+    title: "Um Sonho de Primavera",
+    year: "Livro Futuro",
+    description:
+      "No último livro, Westeros tenta se reerguer após o inverno e a guerra. Os sobreviventes lutam para reconstruir o reino, mas as cicatrizes do passado são profundas. A guerra pelo Trono de Ferro está chegando ao fim, e o destino dos Sete Reinos será finalmente decidido, trazendo esperança ou mais destruição.",
+    image: "assets/images/book-07.jpg",
+    bgImage: "assets/images/book-07-bg.jpg",
+  },
+];
